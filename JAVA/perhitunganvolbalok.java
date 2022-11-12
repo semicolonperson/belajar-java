493b254fa7f56927963ae6c6064646f711f103b3
semicolonperson <115410677+semicolonperson@users.noreply.github.com>
2022-11-12 23:32:48 +0700

    menambahkan file logic java

---
+++ JAVA/perhitunganvolbalok.java
@@ -0,0 +1,46 @@
+package com.example.volumebalokapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private EditText edtWidth;
+    private EditText edtHeight;
+    private EditText edtLength;
+    private Button btnCalculate;
+    private TextView tvResult;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        edtWidth = findViewById(R.id.edt_width);
+        edtHeight = findViewById(R.id.edt_height);
+        edtLength = findViewById(R.id.edt_length);
+        btnCalculate = findViewById(R.id.btn_calculate);
+        tvResult = findViewById(R.id.tv_result);
+        btnCalculate.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId()== R.id.btn_calculate) {
+            String inputLength = edtLength.getText().toString().trim();
+            String inputWidth = edtWidth.getText().toString().trim();
+            String inputHeight = edtHeight.getText().toString().trim();
+
+            Double result = Double.valueOf(inputLength) * Double.valueOf(inputWidth) *  Double.valueOf(inputHeight);
+
+            tvResult.setText (result.toString());
+
+
+        }
+
+    }
+}
